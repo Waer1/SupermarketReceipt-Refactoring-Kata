cfc351e3224bedc0eb0c0ed8fe1dd18a1419cbb3
waer <yousef.elwaer@rabbitmart.com>
2026-01-17 15:40:05 +0200

    introduce ReceiptDiscount

--- typescript/src/model/ReceiptDiscount.ts
+++ typescript/src/model/ReceiptDiscount.ts
@@ -1,3 +1,4 @@
+import { ReceiptPrinter } from "../ReceiptPrinter";
 import { Discount } from "./Discount";
 
 export class ReceiptDiscount {
@@ -5,5 +6,33 @@ export class ReceiptDiscount {
 
     constructor(private readonly discount: Discount) {}
 
+    get productName() {
+        return this.discount.product.name;
+    }
+
+    get discountAmount() {
+        return this.discount.discountAmount;
+    }
+
+    get description() {
+        return this.discount.description;
+    }
+
     
+    getPrintableLine(columns: number): string {
+        let line = "";
+        let productPresentation = this.productName;
+        let pricePresentation = ReceiptPrinter.format2Decimals(this.discountAmount);
+        let description = this.description;
+        line += description;
+        line += "(";
+        line += productPresentation;
+        line += ")";
+        line += ReceiptPrinter.getWhitespace(columns - 3 - productPresentation.length - description.length - pricePresentation.length);
+        line += "-";
+        line += pricePresentation;
+        line += this.EOL;
+        return line;
+    }
+
 }
