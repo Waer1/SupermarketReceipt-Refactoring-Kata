3aef24f243928df4b2ef1747cacfb6ada9ca236c
waer <yousef.elwaer@rabbitmart.com>
2026-01-17 16:15:50 +0200

    introduce allProducts

--- typescript/src/model/IOffer.ts
+++ typescript/src/model/IOffer.ts
@@ -1,6 +1,8 @@
 import { Discount } from "./Discount";
 import { PricedProductQuantity } from "./PricedProductQuantity";
 
+export type PricedProductQuantityMap = { [productName: string]: PricedProductQuantity };
+
 export interface IOffer {
-    getAvailableDiscount(pricedProductQuantity: PricedProductQuantity): Discount | null;
+    getAvailableDiscount(pricedProductQuantity: PricedProductQuantity, allProducts?: PricedProductQuantityMap): Discount | null;
 }
