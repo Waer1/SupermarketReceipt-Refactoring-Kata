d66cf27d80b988b05502a190ff4278ac53079ac3
waer <yousef.elwaer@rabbitmart.com>
2026-01-17 16:46:57 +0200

    compelete the html printer

--- typescript/src/printers/HtmlReceiptPrinter.ts
+++ typescript/src/printers/HtmlReceiptPrinter.ts
@@ -1,10 +1,24 @@
 import { Receipt } from "../model/Receipt";
+import { ReceiptPrinter } from "../ReceiptPrinter";
 import { IReceiptPrinter } from "./ReceiptPrinterInterface";
 
 export class HtmlReceiptPrinter implements IReceiptPrinter {
     constructor(private readonly receipt: Receipt) {}
 
     print(): string {
-        return "";
+        let html = "<ul>";
+
+        for (const item of this.receipt.getItems()) {
+            html += `<li>${item.product.name} - ${ReceiptPrinter.format2Decimals(item.totalPrice)}</li>`;
+        }
+
+        for (const discount of this.receipt.getDiscounts()) {
+            html += `<li>${discount.description}(${discount.product.name}) - -${ReceiptPrinter.format2Decimals(discount.discountAmount)}</li>`;
+        }
+
+        html += "</ul>";
+        html += `<p>Total: ${ReceiptPrinter.format2Decimals(this.receipt.getTotalPrice())}</p>`;
+
+        return html;
     }
 }
